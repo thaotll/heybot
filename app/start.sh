0c21ed6d41b10df3c98e4d345900935840f4984c
Thao Stein <thaostein1@gmail.com>
2025-05-16 05:10:01 +0200

    Clean up Helm-generated files and improve handling of templated manifests

--- app/start.sh
+++ app/start.sh
@@ -7,5 +7,8 @@ echo "Using CURRENT_COMMIT_ID=$CURRENT_COMMIT_ID"
 echo "▶️ Running main.py..."
 python3 /app/main.py
 
+echo "▶️ Running bazinga_cve_bot.py..."
+python3 /app/bazinga_cve_bot.py
+
 # 4) Container am Leben halten (optional, wenn nötig)
 # sleep infinity
